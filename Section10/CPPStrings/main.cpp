949c71260899bf588a5adddc063d1e113404340c
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2021-02-01 13:50:19 -0700

    c++ string tests

--- Section10/CPPStrings/main.cpp
+++ Section10/CPPStrings/main.cpp
@@ -1,9 +1,17 @@
-#include <stdio.h>
+#include <iostream>
 # include <string>
 
 using namespace std;
 int main(int argc, char **argv)
 {
-	
+    //was mostly just checking to see if equal meant same object or equality
+	string s1 {"apple"};
+    string s2 {"apple"};
+    string s3 {s1};
+    bool val = (s1==s2);
+    bool val2 = (s3==s1);
+    cout <<boolalpha;
+    cout << val << endl;
+    cout <<val2 << endl;
 	return 0;
 }
