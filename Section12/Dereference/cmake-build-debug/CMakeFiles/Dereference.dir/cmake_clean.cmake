ce068402be773bbb8b07a676d5ca417bef4a003a
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2021-02-02 14:23:52 -0700

    Added Dereference examples

---
+++ Section12/Dereference/cmake-build-debug/CMakeFiles/Dereference.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dereference.dir/main.cpp.obj"
+  "Dereference.exe"
+  "Dereference.exe.manifest"
+  "Dereference.pdb"
+  "libDereference.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dereference.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
