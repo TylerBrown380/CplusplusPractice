4ff51520cbf95d61a711104a0e38b5c8e9426b52
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2021-02-02 14:59:00 -0700

    Added project that shows how closely related arrays and pointers are

---
+++ Section12/ArraysAndPointers/cmake-build-debug/CMakeFiles/ArraysAndPointers.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "ArraysAndPointers.exe"
+  "ArraysAndPointers.exe.manifest"
+  "ArraysAndPointers.pdb"
+  "CMakeFiles/ArraysAndPointers.dir/main.cpp.obj"
+  "libArraysAndPointers.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArraysAndPointers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
