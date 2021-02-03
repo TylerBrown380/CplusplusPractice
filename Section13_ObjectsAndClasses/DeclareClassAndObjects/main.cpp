d548e9976b8824822e6f717b6f10b0c746bbb57c
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2021-02-03 12:03:15 -0700

    Adding more examples with class manipulation

--- Section13_ObjectsAndClasses/DeclareClassAndObjects/main.cpp
+++ Section13_ObjectsAndClasses/DeclareClassAndObjects/main.cpp
@@ -4,37 +4,47 @@
 
 
 class Player {
+public:
     //attributes
     std::string name {"Player"};
     int health {100};
     int xp {5};
 
     //methods
-    void talk(std::string);
+    void talk(std::string s) {std::cout << name << " says " << s << std::endl;}
     bool is_dead();
 };
 
 class Account {
-    std::string name {"Account"};
-    double balance {0.0};
+public:
+    std::string name;
+    double balance;
 
     //methods
-    bool deposit(double);
-    bool withdraw(double);
+    bool deposit(double bal){balance +=bal; std::cout << "Depositing amount: " << bal<< std::endl;}
+    bool withdraw(double bal){balance -= bal; std::cout << "Withdrawing amount: " << bal << std::endl;}
 };
 int main() {
     Account tyler_account;
-    Account brook_account;
-    Player tyler;
-    Player brook;
-    Player players[] {tyler, brook};
+    tyler_account.name = "Tyler's account";
+    tyler_account.balance = 5000.0;
+    tyler_account.deposit(500.0);
+    std::cout << tyler_account.balance << std::endl;
+    tyler_account.withdraw(8000.5);
+    std::cout << tyler_account.balance << std::endl;
 
-    std::vector<Player> player_vec{tyler};
-    player_vec.push_back(brook);
 
-    Player *enemy{new Player};
+    Player player1;
+    player1.name = "Tyler";
+    player1.health = 1000;
+    player1.xp = 100;
+    player1.talk("Hello");
 
-    delete enemy;
+    Player *enemy {new Player};
+    (*enemy).name = "Enemy";
+    (*enemy).health = 20;
+    enemy->xp = 15;
+    enemy->talk("Sup kid");
     return 0;
 }
 
