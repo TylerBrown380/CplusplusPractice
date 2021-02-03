f244e563dae390b912a9c807211d7c27ad32411f
Tyler Brown <Tylerbrown380@u.boisestate.edu>
2021-02-03 16:06:11 -0700

    Breaking everything into different classes.

---
+++ Section13_ObjectsAndClasses/ImplementingMethods2/Account.cpp
@@ -0,0 +1,18 @@
+#include "Account.h"
+#include <iostream>
+#include <string>
+void Account::set_name(std::string s) { name = s;}
+
+std::string Account::get_name() {return name;}
+
+bool Account::deposit(double amount) {
+    balance += amount;
+    return true;
+}
+
+bool Account::withdraw(double amount) {
+    if(balance-amount >= 0){
+        balance -=amount;
+        return true;
+    } else {return false;}
+}
